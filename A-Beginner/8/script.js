9c1127427ec188a0459435c130f41edce84402ed
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-10-12 13:30:37 +0200

    solve-8

---
+++ A-Beginner/8/script.js
@@ -0,0 +1,8 @@
+// Skapa ett objekt som representerar dig själv och som innehåller properties firstName, lastName och age
+
+let firstName = 'Theodor'
+let lastName = 'Aifadis'
+let age = 17
+
+const identity = {firstName, lastName, age}
+ 
