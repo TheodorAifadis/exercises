35ee4b86a73743b8fe4d7fd7f5852fd84d5fc0ad
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-10-12 14:18:52 +0200

    solve-16

--- A-Beginner/16/script.js
+++ A-Beginner/16/script.js
@@ -1,8 +1,12 @@
 // Skriv en funktion checkAge som accepterar ett tal age som argument 
 // och returnerar strängen "old" om age är större än 65 annars returnerar "young"
 
-console.log(checkAge(5))
+console.log(checkAge(70))
 
-function checkAge(a) {
-    return a > 65 ? 'old' : 'young'
+function checkAge(age) {
+    if (age > 65) {
+        return 'old'
+    } else {
+        return 'young'
+    }
 }
