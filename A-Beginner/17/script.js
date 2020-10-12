578b9c822f9953ba0fdab052b7e24a20cf38ed22
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-10-12 14:30:42 +0200

    solve-17

---
+++ A-Beginner/17/script.js
@@ -0,0 +1,11 @@
+// Skriv en funktion isTheo som returnerar true ifall argumentet är strängen "Theo" och annars false
+
+console.log(isTheo('Theo'))
+
+function isTheo(a){
+    if (a == 'Theo') {
+        return 'true'
+    } else {
+        return 'false'
+    }
+}
