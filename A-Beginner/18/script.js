046abc6dd0b91cd8204dd97bd8ed5210ecf77661
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-10-12 14:37:20 +0200

    solve-18

---
+++ A-Beginner/18/script.js
@@ -0,0 +1,7 @@
+// Skriv en funktion som kollar ifall två strängar är lika (och returnerar true i så fall, annars false)
+
+console.log(alike('x', 'y'))
+
+function alike(a, b) {
+    return a == b ? 'true' : 'false'
+}
