54002d4d8b1f6a2699f173d10f515eba36b356d0
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-10-16 17:17:37 +0200

    solve-23

---
+++ A-Beginner/23/script.js
@@ -0,0 +1,7 @@
+// Skriv en funktion isDivisibleByFive som returnerar true/false om argumentet är delbart med 5
+
+function isDivisibleByFive(a) {
+    return a % 5 === 0
+}
+
+console.log(isDivisibleByFive(15))
