677f44d6ee211aa0a44b5f05b2d3ab093c7e1706
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-10-16 17:12:12 +0200

    solve-21

---
+++ A-Beginner/21/script.js
@@ -0,0 +1,7 @@
+// Skriv en funktion isEven som returnerar true om argumentet är jämnt och false om argumentet är udda
+
+function isEven(x) {
+    return x % 2 === 0
+}
+
+console.log(isEven(4))
---
+++ A-Beginner/22/script.js
@@ -0,0 +1,7 @@
+// Skriv funktionen isOdd
+
+console.log(isEven(6));
+
+function isEven(x) {
+    return x % 2 != 1;
+}
